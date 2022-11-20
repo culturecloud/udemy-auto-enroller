3be0a297f9af662ab1a172c022cc800b1475d21d
RedoX <pseudokawaii@gmail.com>
2022-11-20 05:59:25 +0000

    Getting started

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,19 @@
+import sys
 import time
+import logging
 from pyrogram import Client
-from app.config import var
+from os import getenv, environ
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class env(object):
+    API_ID = int(getenv('API_ID'))
+    API_HASH = str(getenv('API_HASH'))
+    # BOT_TOKEN = str(getenv('BOT_TOKEN'))
+    # OWNER_ID = int(getenv('OWNER_ID'))
+    # BIN_CHANNEL = int(getenv('BIN_CHANNEL'))
+    FQDN = f"https://{getenv('REPL_SLUG')}.{getenv('REPL_OWNER')}.repl.co"
 
 StartTime = time.time()
 plugins = dict(
@@ -11,10 +24,22 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 logging.basicConfig(format="[%(levelname)s] [%(filename)s] [%(funcName)s] [%(lineno)d] %(message)s")
 
-bot = Client(
-    session_name=':memory:',
-    api_id=var.API_ID,
-    api_hash=var.API_HASH,
-    bot_token=var.BOT_TOKEN,
+def generate_session():
+    with Client("user", api_id=env.API_ID, api_hash=env.API_HASH, in_memory=True) as user:
+        user.send_message("me", f"`{user.export_session_string()}`")
+        print("\nSet the value as SESSION_STRING.")
+
+if 'SESSION_STRING' not in environ:
+    generate_session()
+    sys.exit()
+
+user = Client(
+    "user",
+    api_id=env.API_ID,
+    api_hash=env.API_HASH,
+    session_string=getenv("SESSION_STRING"),
     plugins=plugins
 )
+
+with user as first_dive:
+    user_dict = first_dive.get_me()
--- app/plugins/handlers.py
+++ app/plugins/handlers.py
@@ -1,5 +1,10 @@
+import requests
 from pyrogram import Client, filters
 
-@Client.on_message(filters.command('start'))
-def start_handler(_, msg):
-    msg.reply("Hello! You\'ve reached **Pyrogram Playbot**")
+@Client.on_message(filters.chat('TestingChannel404'))
+def link_parser(_, msg):
+    button = list(msg.reply_markup.inline_keyboard)
+    coursedb_link = button[0][0].url
+    udemy_link = (requests.get(coursedb_link)).url
+    print(udemy_link)
+    
